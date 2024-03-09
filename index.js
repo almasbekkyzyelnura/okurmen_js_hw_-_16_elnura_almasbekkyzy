336fc5db1fe396a9b321c32f698b38d93895ced5
almasbekkyzyelnura <elnuraalmasbekkyzy8@gmail.com>
2024-03-10 00:57:46 +0600

    тапшырма 16

---
+++ index.js
@@ -0,0 +1,61 @@
+
+ const number = 6
+
+
+switch (number) {
+    case 1:
+        console.log('Дуйшомбу');
+        break;
+    case 2:
+        console.log('Шаршенби');
+        break;
+    case 3:
+        console.log('Шейшенби');
+        break;
+    case 4:
+        console.log('Бейшемби');
+        break;
+    case 5:
+        console.log('Жума');
+        break;
+    case 6:
+        console.log('Ишенби');
+        break;
+    case 7:
+        console.log('Жекшенби');
+        break;
+    default:
+        console.log('Number');
+} 
+
+/*const number = +prompt('0-дон  5-ке чейинки сан жазгыла', '');
+
+switch (number) {
+    case 0:
+        alert("Силер 0 санын жаздынар");
+        break;
+    case 1:
+        alert("Силер 1 санын жаздынар");
+        break;
+    case 2:
+    case 3 :   
+        alert("Силер 2 же 3 санын жаздынар");
+        break;
+    case 4 :
+    case 5:
+        alert("Силер 4 же 5 санын жаздынар");
+        break;
+} */
+
+
+
+/*const browser = prompt ('Браузердтн атын жазгыла','')
+
+
+if (browser === 'Edge') {
+    alert("Бул сенин браузерин!");
+  } else if (browser === 'Chrome'|| browser === 'Firefox'|| browser === 'Safari'|| browser === 'Opera') {
+    alert( 'Бул Chrome || Firefox || Safari || Opera' );
+  } else {
+    alert( 'Бул сенин браузерин!' );
+  }*/
